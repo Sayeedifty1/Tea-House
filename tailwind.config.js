8a99a4475aab5b7c79d527080dbfd699f6031087
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-03-15 00:47:34 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'orange-light': '#ff0000',
+        'orange-dark': '#ff8939',
+        
+      },
+    },
+  }
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'cup': "url('./images/client')",
+    
+        }
+      }
+    }
+  }
